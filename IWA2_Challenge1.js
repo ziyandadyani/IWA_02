d70aa2c9fcc10c0b088e28973002b139c9fb4c65
Ziyanda Dyani <ziyandadyani17@gmail.com>
2023-03-30 08:26:38 +0200

    Corrected syntax errors in .js code for challenge 2.1

---
+++ IWA2_Challenge1.js
@@ -0,0 +1,11 @@
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */ const surname = 'Smith'
+
+
+console.log(user, surname)
+
+// the syntax for multi-line comments was incorrect so all the code following the asterisks was  commented out 
